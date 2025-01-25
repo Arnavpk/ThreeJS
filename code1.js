07aebbb159428e7014b977a104d740053d256245
Arnav Kulkarni <arnavkulkarni@Arnavs-MacBook-Air.local>
2025-01-25 17:16:56 +0530

    first commit

---
+++ code1.js
@@ -0,0 +1,15 @@
+var firstname = "Arnav";
+var lastname = "Kulkarni";
+var email = "arnavpk@gmail.com";
+var mobilenumber = "9699329931";
+var country = "India";
+var state = "Maharashtra";
+var isLoggedInFromChorme = false;
+
+console.log(`
+    My name is ${firstname} 
+    My surname is ${lastname}
+    Contact me on ${mobilenumber}
+    Or you can mail me on ${email}
+    I am from ${country}
+    `)
