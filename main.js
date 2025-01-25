a8ac8f9327c96e00c8088583c8e9600bd70e8207
Arnav Kulkarni <arnavkulkarni@Arnavs-MacBook-Air.local>
2025-01-25 17:26:22 +0530

    Colour changed to white

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 3, 3, 3 );
-const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -17,8 +17,8 @@ camera.position.z = 5;
 
 function animate() {
 
-	cube.rotation.x += 0.05;
-	cube.rotation.y += 0.05;
+	cube.rotation.x += 0.03;
+	cube.rotation.y += 0.03;
 
 	renderer.render( scene, camera );
 
